eed9eb6db77e4cf1d58bcde25ea27eff0e946030
Sangmin Na <69471244+sangmin-iam@users.noreply.github.com>
2025-05-22 02:20:09 +0900

    Time: 18 ms (5.06%), Space: 20.5 MB (16.89%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,21 +1,28 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
+        # 1. count
         count = defaultdict(int)
 
+        # 2. for loop
         for n in nums:
             count[n] += 1
-
-            if len(count) <= 2:
-                continue
             
-            new_count = defaultdict(int)
-            for n, c in count.items():
-                if c > 1:
-                    new_count[n] = c - 1
-            count = new_count
+            # n // 3 -> 최대 2개만 가능
+            if len(count) > 2:
+                new_count = defaultdict(int)
+
+                # cnt가 0이거나 1인 경우는 제외
+                for n, cnt in count.items():
+                    if cnt > 1:
+                        new_count[n] = cnt - 1
+                
+                count = new_count
         
+        # 3. 마지막 체크
         res = []
-        for n in count:
+        for n in count.keys():
             if nums.count(n) > len(nums) // 3:
                 res.append(n)
+
         return res
+        
