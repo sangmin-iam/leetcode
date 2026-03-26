2f657b6c0dd59ccf0424154a9ad7d10fa0ebc86b
Sam (Sangmin) Na <69471244+sangmin-iam@users.noreply.github.com>
2026-03-26 13:00:30 -0700

    Time: 52 ms (46.17%), Space: 25.2 MB (66.41%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,24 +1,20 @@
 # TC: O(n log n)
-# SC: O(1)
+# SC: O(1) or O(n)
+
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        # Limit: Maximum 3
-        # People: Weight [3, 2, 2, 1]
-        # 사람들의 Weight는 항상 Limit보다 작거나 같음
-        # 1. 정렬 [1, 2, 2, 3]
-        people.sort()
-    
-        # 2. Two Pointer
+        people.sort() # O(n log n)
+
         l = 0
         r = len(people) - 1
         count = 0
-        
-        while l <= r:
-            remaining = limit - people[r]
+
+        while l <= r: # O(n)
+            if people[l] + people[r] <= limit:
+                l += 1
+            
             r -= 1
             count += 1
-
-            if remaining >= people[l] and l <= r:
-                l += 1
         
         return count
+    
