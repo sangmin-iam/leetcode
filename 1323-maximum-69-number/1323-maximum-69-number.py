1e7ecd150ac97cc86ebead9cc145919ca23a5a4c
Sam (Sangmin) Na <69471244+sangmin-iam@users.noreply.github.com>
2026-03-27 13:19:40 -0700

    Time: 0 ms (100%), Space: 19.3 MB (40.51%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -0,0 +1,18 @@
+# 1. 문제 이해
+# - 6과 9로만 이루어진 숫자
+# - 0~1번 숫자 변경 가능
+# - 최대 숫자 리턴하기
+
+# 2. 문제 설계
+# - 가장 왼쪽부터 보면서 6이 있으면 9로 변경
+# - 이 작업을 위해서 String으로 변경 필요
+
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        stringNum = str(num)
+        n = len(stringNum)
+        for i, digit in enumerate(stringNum):
+            if digit == '6':
+                return int(stringNum[:i] + '9' + stringNum[i + 1:n])
+        
+        return num
